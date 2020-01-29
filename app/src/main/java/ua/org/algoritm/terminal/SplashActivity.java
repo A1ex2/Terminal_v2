d07d76fa42a181087c30dcd884c7672b27fe0ba0
a1ex <jaroshov@ukr.net>
2020-01-29 21:15:15 +0200

    29012020

--- app/src/main/java/ua/org/algoritm/terminal/SplashActivity.java
+++ app/src/main/java/ua/org/algoritm/terminal/SplashActivity.java
@@ -2,9 +2,7 @@ package ua.org.algoritm.terminal;
 
 import android.content.Intent;
 import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import ua.org.algoritm.terminal.Activity.Password;
 
 public class SplashActivity extends AppCompatActivity {
