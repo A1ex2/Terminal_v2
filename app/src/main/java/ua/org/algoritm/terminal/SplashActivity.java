03014767c15f34314ea2037d2a2da39bbb9f8676
a1ex <jaroshov@ukr.net>
2020-01-29 16:12:31 +0200

    26012020

---
+++ app/src/main/java/ua/org/algoritm/terminal/SplashActivity.java
@@ -0,0 +1,20 @@
+package ua.org.algoritm.terminal;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import ua.org.algoritm.terminal.Activity.Password;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(this, Password.class);
+        startActivity(intent);
+        finish();
+    }
+}
