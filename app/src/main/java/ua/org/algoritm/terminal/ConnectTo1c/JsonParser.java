c80df5e1ab5b8c2353a189e3e4258b1a38a17eb8
a1ex <jaroshov@ukr.net>
2020-02-19 10:43:14 +0200

    19022020

--- app/src/main/java/ua/org/algoritm/terminal/ConnectTo1c/JsonParser.java
+++ app/src/main/java/ua/org/algoritm/terminal/ConnectTo1c/JsonParser.java
@@ -7,17 +7,18 @@ import org.json.JSONObject;
 import java.util.ArrayList;
 
 import ua.org.algoritm.terminal.Objects.CarDataOutfit;
+import ua.org.algoritm.terminal.Objects.OperationOutfits;
 import ua.org.algoritm.terminal.Objects.OrderOutfit;
 
 public class JsonParser {
 
     public static ArrayList<OrderOutfit> getOrderOutfit(String response) throws JSONException {
         ArrayList<OrderOutfit> mOrderOutfits = new ArrayList<>();
-        JSONArray orders = new JSONArray(response);
+        JSONArray ordersJSON = new JSONArray(response);
 
-        for (int i = 0; i < orders.length(); i++) {
+        for (int i = 0; i < ordersJSON.length(); i++) {
             OrderOutfit orderOutfit = new OrderOutfit();
-            JSONObject order = new JSONObject(orders.get(i).toString());
+            JSONObject order = new JSONObject(ordersJSON.get(i).toString());
 
             orderOutfit.setID(order.getString("ID"));
             orderOutfit.setDescription(order.getString("Description"));
@@ -26,10 +27,10 @@ public class JsonParser {
             orderOutfit.setStateID(order.getString("StateID"));
             orderOutfit.setState(order.getString("State"));
 
-            JSONArray CarDataOutfits = new JSONArray(order.getString("CarDataOutfit"));
+            JSONArray CarDataOutfitsJSON = new JSONArray(order.getString("CarDataOutfit"));
             ArrayList<CarDataOutfit> mCarDataOutfits = new ArrayList<>();
-            for (int j = 0; j < CarDataOutfits.length(); j++) {
-                JSONObject car = new JSONObject(CarDataOutfits.get(j).toString());
+            for (int j = 0; j < CarDataOutfitsJSON.length(); j++) {
+                JSONObject car = new JSONObject(CarDataOutfitsJSON.get(j).toString());
 
                 CarDataOutfit mCar = new CarDataOutfit();
                 mCar.setCarID(car.getString("CarID"));
@@ -37,11 +38,27 @@ public class JsonParser {
                 mCar.setSectorID(car.getString("SectorID"));
                 mCar.setSector(car.getString("Sector"));
                 mCar.setRow(car.getString("Row"));
-               // mCar.setBarCode(car.getString("BarCode"));
+                mCar.setBarCode(car.getString("BarCode"));
+
+                JSONArray OperationsJSON = new JSONArray(car.getString("Operations"));
+                ArrayList<OperationOutfits> mOperation = new ArrayList<>();
+                for (int k = 0; k < OperationsJSON.length(); k++) {
+                    JSONObject operation = new JSONObject(OperationsJSON.get(k).toString());
+
+                    OperationOutfits mOperationOutfits = new OperationOutfits();
+                    mOperationOutfits.setOperationID(operation.getString("OperationID"));
+                    mOperationOutfits.setOperation(operation.getString("Operation"));
+
+                    mOperation.add(mOperationOutfits);
+                }
+
+                mCar.setOperations(mOperation);
 
                 mCarDataOutfits.add(mCar);
             }
             orderOutfit.setCarDataOutfit(mCarDataOutfits);
+
+            mOrderOutfits.add(orderOutfit);
         }
         return mOrderOutfits;
     }
